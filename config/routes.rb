c560d425fe41776add6b9f10a3f67102ffc522f1
kyuya5655 <134293910+kyuya5655@users.noreply.github.com>
2023-10-10 16:06:28 +0000

    20231011

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   # homes
@@ -7,9 +7,10 @@ Rails.application.routes.draw do
   get '/about' => 'homes#about'
 
   # users
+
   resources :users, only: [:show, :edit, :update]
-  get '/user/quit_check' => 'users#quit_check'
-  patch '/user/withdraw' => 'users#withdraw'
+  get '/users/quit_check' => 'users#quit_check'
+  patch '/users/withdraw' => 'users#withdraw'
 
   # tasks
   resources :tasks, only: [:index, :new, :create, :edit, :update]
--- db/schema.rb
+++ db/schema.rb
@@ -32,21 +32,4 @@ ActiveRecord::Schema.define(version: 2023_10_09_112451) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.string "last_name", null: false
-    t.string "first_name", null: false
-    t.string "last_name_kana", null: false
-    t.string "first_name_kana", null: false
-    t.boolean "is_deleted", default: false, null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  end
-
 end
