e6d2f2e5dd8f9a7e204e0d701e3ba279ca55e34c
kyuya5655 <134293910+kyuya5655@users.noreply.github.com>
2023-10-14 04:19:25 +0000

    20231014.2

--- db/schema.rb
+++ db/schema.rb
@@ -59,10 +59,7 @@ ActiveRecord::Schema.define(version: 2023_10_12_213233) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "last_name", null: false
-    t.string "first_name", null: false
-    t.string "last_name_kana", null: false
-    t.string "first_name_kana", null: false
+    t.string "name", null: false
     t.boolean "is_deleted", default: false, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
